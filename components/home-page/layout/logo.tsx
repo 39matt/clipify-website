dc7dbad56c51abca283448577cc34e7506838dea
39matt <matija.todosijevic.i4@gmail.com>
2025-06-11 10:00:47 +0200

    edit: front page

--- components/home-page/layout/logo.tsx
+++ components/home-page/layout/logo.tsx
@@ -23,7 +23,7 @@ export const Logo = ({ href = '/', onClick }: LogoProps) => {
   }
 
   return (
-    <Flex h="8" flexShrink="0" alignItems="flex-start">
+    <Flex h="20" flexShrink="0" alignItems="flex-start">
       <Link
         href={href}
         display="flex"
