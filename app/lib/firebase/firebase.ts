3dee7370e20b7be697a9a0a8a333b9ac9383280f
39matt <matija.todosijevic.i4@gmail.com>
2025-06-11 16:56:28 +0200

    fix: build 3

--- app/lib/firebase/firebase.ts
+++ app/lib/firebase/firebase.ts
@@ -1,24 +1,25 @@
 import { getApp, getApps, initializeApp } from '@firebase/app'
 import { getFirestore } from '@firebase/firestore'
-import { getAuth, signOut } from '@firebase/auth'
+import { getAuth } from '@firebase/auth'
 
 
 const firebaseConfig = {
 
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
 
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 
-  appId: process.env.FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 
 };
 
+console.log("AAAAAAAA " + process.env.FIREBASE_API_KEY!)
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
