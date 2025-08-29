5aa6a3cc9b3b32af8499f25044594319f52ddb87
39matt <matija.todosijevic.i4@gmail.com>
2025-08-29 11:44:18 +0200

    fix: cached data for campaigns

--- app/api/campaign/get-all/route.ts
+++ app/api/campaign/get-all/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { adminAuth, adminDb } from '../../../lib/firebase/firebaseAdmin'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   try {
     const snapshot = await adminDb.collection('campaigns').get();
