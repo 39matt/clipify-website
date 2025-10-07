6c19f3302ab30019c3517c3e51714ef3f53de63a
39matt <matija.todosijevic.i4@gmail.com>
2025-10-07 17:55:36 +0200

    fix: theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ export default function Layout(props: { children: React.ReactNode }) {
   const colorMode = 'dark'
 
   return (
-    <html lang="en" data-theme={colorMode} style={{ colorScheme: colorMode }}>
+    <html lang="en" data-theme={colorMode} style={{ colorScheme: colorMode, backgroundColor: '#0f172a' }} suppressHydrationWarning>
       <head>
         <title>Clipify</title>
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
       </head>
-      <body className={`chakra-ui-${colorMode}`}>
+      <body className={`chakra-ui-${colorMode}`} style={{ backgroundColor: '#0f172a', color: 'white' }}>
         <ColorModeScript initialColorMode={colorMode} />
         <Provider>{props.children}</Provider>
       </body>
