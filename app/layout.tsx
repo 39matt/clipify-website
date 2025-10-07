be8b6325aa3949bf3e37ab9b95f250e0be3034c8
39matt <matija.todosijevic.i4@gmail.com>
2025-10-07 16:55:35 +0200

    fix: theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import { ColorModeScript, theme } from '@chakra-ui/react'
+import { ColorModeScript } from '@chakra-ui/react'
+import { theme } from '#theme'
 
 import { Provider } from './providers/provider'
 
