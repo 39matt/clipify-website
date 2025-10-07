4939d84f735f8102494e230249e0ce505fdc3696
39matt <matija.todosijevic.i4@gmail.com>
2025-10-07 20:56:17 +0200

    fix: theme v5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,19 @@
 import { ColorModeScript } from '@chakra-ui/react'
-import { cookies } from 'next/headers'
 
 import { Provider } from './providers/provider'
 
 export default function Layout(props: { children: React.ReactNode }) {
   const colorMode = 'dark'
-  const cookieStore = cookies()
-  const cookieString = cookieStore.toString()
 
   return (
-    <html lang="en" data-theme={colorMode} style={{ colorScheme: colorMode }} className={`chakra-ui-${colorMode}`} suppressHydrationWarning>
+    <html lang="en" data-theme={colorMode} style={{ colorScheme: colorMode }}>
       <head>
         <title>Clipify</title>
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
       </head>
       <body className={`chakra-ui-${colorMode}`}>
         <ColorModeScript initialColorMode={colorMode} />
-        <Provider cookies={cookieString}>{props.children}</Provider>
+        <Provider>{props.children}</Provider>
       </body>
     </html>
   )
