7dfcb66cd0a87b075162f53343926d5fde1ea300
39matt <matija.todosijevic.i4@gmail.com>
2025-11-11 14:36:37 +0100

    feat: sort for last update

--- app/(pages)/dashboard/admin/campaign/[id]/all-videos/AllVideosAdminPage.tsx
+++ app/(pages)/dashboard/admin/campaign/[id]/all-videos/AllVideosAdminPage.tsx
@@ -3,17 +3,24 @@
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import {
-  Box, Button,
+  Box,
+  Button,
   Center,
   Divider,
-  Heading, HStack, SimpleGrid,
-  Spinner, Stat, StatLabel, StatNumber,
-  Text, useToast,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Spinner,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+  useToast,
   VStack,
-} from '@chakra-ui/react'
-import AdminVideoCard from './components/AdminVideoCard'
-import { ICampaign } from '../../../../../../lib/models/campaign'
-import { IVideo } from '../../../../../../lib/models/video'
+} from '@chakra-ui/react';
+import AdminVideoCard from './components/AdminVideoCard';
+import { ICampaign } from '../../../../../../lib/models/campaign';
+import { IVideo } from '../../../../../../lib/models/video';
 
 interface AdminCampaignPageProps {
   idToken: string;
@@ -26,6 +33,9 @@ const AllVideosAdminPage: React.FC<AdminCampaignPageProps> = ({ idToken }) => {
   const [videos, setVideos] = useState<IVideo[] | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [sortOption, setSortOption] = useState<'createdAt' | 'lastUpdatedAt'>(
+    'createdAt'
+  );
   const toast = useToast();
 
   useEffect(() => {
@@ -42,7 +52,13 @@ const AllVideosAdminPage: React.FC<AdminCampaignPageProps> = ({ idToken }) => {
         }).then((res) => res.json());
 
         setCampaign(responseJson['campaign']);
-        setVideos((responseJson['videos'] as IVideo[]).sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
+        setVideos(
+          (responseJson['videos'] as IVideo[]).sort(
+            (a, b) =>
+              new Date(b.createdAt).getTime() -
+              new Date(a.createdAt).getTime()
+          )
+        );
         setLoading(false);
       } catch (err) {
         setLoading(false);
@@ -56,36 +72,184 @@ const AllVideosAdminPage: React.FC<AdminCampaignPageProps> = ({ idToken }) => {
 
   const handleDeleteVideo = async (videoId: string) => {
     try {
-      const res = await fetch(
-        `/api/campaign/video/delete`,
-        { method: 'DELETE',
-          headers: { 'Content-Type': 'multipart/form-data' },
-          body: JSON.stringify({ videoId: videoId, campaignId: campaignId }),
-        }
-      );
+      const res = await fetch(`/api/campaign/video/delete`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: JSON.stringify({ videoId: videoId, campaignId: campaignId }),
+      });
+
       if (!res.ok) {
         toast({
           title: 'Failed!',
           description: 'Failed to remove the video.',
-          status: 'error', // "success" | "error" | "warning" | "info"
-          duration: 3000, // milliseconds
+          status: 'error',
+          duration: 3000,
           isClosable: true,
-          position: 'top-right', // "top", "top-right", "bottom-left", etc.
+          position: 'top-right',
         });
         return;
       }
+
       toast({
         title: 'Video deleted.',
         description: 'The video has been successfully removed.',
-        status: 'success', // "success" | "error" | "warning" | "info"
-        duration: 3000, // milliseconds
+        status: 'success',
+        duration: 3000,
         isClosable: true,
-        position: 'top-right', // "top", "top-right", "bottom-left", etc.
+        position: 'top-right',
       });
+
       setVideos((prev) => prev?.filter((v) => v.id !== videoId) || null);
     } catch (err) {
       console.error(err);
       setError('Error deleting video: ' + err);
+    }
+  };
+
+  const handleUpdateSingleVideo = async (video: IVideo) => {
+    try {
+      const getVideoResponse = await fetch('/api/campaign/video/get-info', {
+        method: 'PUT',
+        body: JSON.stringify({
+          platform: video.link.includes('instagram') ? 'Instagram' : 'TikTok',
+          videoId: video.link.split('/')[5],
+          videoUrl: video.link,
+          api_key: process.env.NEXT_PUBLIC_RAPIDAPI_KEY!,
+        }),
+      });
+
+      if (getVideoResponse.status !== 200) {
+        toast({
+          title: 'Error!',
+          description: `Error getting video info for \n${video.name}`,
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+        });
+        return;
+      }
+
+      const getVideoResponseJson = await getVideoResponse.json();
+      const newVideo = getVideoResponseJson['videoInfo'] as IVideo;
+
+      if (!newVideo || !newVideo.accountName) {
+        toast({
+          title: 'Skipped.',
+          description: `No valid data for ${video.name}.`,
+          status: 'info',
+          duration: 2500,
+          isClosable: true,
+          position: 'top-right',
+        });
+        return;
+      }
+
+      await fetch('/api/campaign/video/update-info', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${idToken}`,
+        },
+        body: JSON.stringify({ video: newVideo, campaignId }),
+      });
+
+      setVideos((prev) =>
+        prev
+          ? prev.map((v) =>
+            v.id === video.id ? { ...v, ...newVideo } : v
+          )
+          : null
+      );
+
+      toast({
+        title: 'Updated!',
+        description: `Successfully updated ${video.name}`,
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+        position: 'top-right',
+      });
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: 'Error updating views!',
+        description: `${video.name}`,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+        position: 'top-right',
+      });
+    }
+  };
+
+  const handleUpdateViews = async () => {
+    try {
+      const updatedVideos: IVideo[] = [];
+      for (const video of videos || []) {
+        const getVideoResponse = await fetch('/api/campaign/video/get-info', {
+          method: 'PUT',
+          body: JSON.stringify({
+            platform: video.link.includes('instagram') ? 'Instagram' : 'TikTok',
+            videoId: video.link.split('/')[5],
+            videoUrl: video.link,
+            api_key: process.env.NEXT_PUBLIC_RAPIDAPI_KEY!,
+          }),
+        });
+
+        if (getVideoResponse.status !== 200) {
+          toast({
+            title: 'Error!',
+            description: `Error getting video info for \n${video.name}`,
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+            position: 'top-right',
+          });
+          continue;
+        }
+
+        const getVideoResponseJson = await getVideoResponse.json();
+        const newVideo = getVideoResponseJson['videoInfo'] as IVideo;
+
+        if (!newVideo || !newVideo.accountName) continue;
+
+        await fetch('/api/campaign/video/update-info', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${idToken}`,
+          },
+          body: JSON.stringify({ video: newVideo, campaignId }),
+        });
+
+        updatedVideos.push(newVideo);
+        toast({
+          title: 'Success!',
+          description: `Successfully updated video views for \n${video.name}`,
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+        });
+      }
+
+      setVideos(updatedVideos);
+      await fetch('/api/campaign/calculate-progress', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ campaignId }),
+      });
+    } catch (error) {
+      console.error(error);
+      toast({
+        title: 'Error!',
+        description: 'Error while updating video views.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+        position: 'top-right',
+      });
     }
   };
 
@@ -120,189 +284,19 @@ const AllVideosAdminPage: React.FC<AdminCampaignPageProps> = ({ idToken }) => {
     );
   }
 
-  const handleUpdateViews = async () => {
-    try {
-      const updatedVideos:IVideo[] = [];
-      for (const video of videos || []) {
-        const getVideoResponse = await fetch('/api/campaign/video/get-info',
-          {
-            method: "PUT",
-            body: JSON.stringify({
-              platform:video.link.includes("instagram") ? "Instagram" : "TikTok",
-              videoId: video.link.split('/')[5],
-              videoUrl: video.link,
-              api_key:process.env.NEXT_PUBLIC_RAPIDAPI_KEY!
-            })
-          }
-        );
-
-        if (getVideoResponse.status !== 200) {
-          console.log("Error getting video info for: ", video)
-          toast({
-            title: 'Error!',
-            description: `Error getting video info for \n${video.name}`,
-            status: 'error', // "success" | "error" | "warning" | "info"
-            duration: 3000, // milliseconds
-            isClosable: true,
-            position: 'top-right', // "top", "top-right", "bottom-left", etc.
-          });
-          continue;
-        }
-
-        const getVideoResponseJson = await getVideoResponse.json();
-        const newVideo = getVideoResponseJson['videoInfo'] as IVideo;
-
-        // if(!newVideo) {
-        //   const deleteVideoResponse = await fetch('/api/campaign/video/delete', {
-        //     method: "DELETE",
-        //     body: JSON.stringify({
-        //       videoId: video.id,
-        //       campaignId: campaign.id,
-        //     })
-        //   })
-        //   if (deleteVideoResponse.status !== 200) {
-        //     toast({
-        //       title: 'Error',
-        //       description: `Error deleting video \n${video.name}`,
-        //       status: 'error', // "success" | "error" | "warning" | "info"
-        //       duration: 3000, // milliseconds
-        //       isClosable: true,
-        //       position: 'top-right', // "top", "top-right", "bottom-left", etc.
-        //     });
-        //     continue;
-        //   }
-        //   toast({
-        //     title: 'Success',
-        //     description: `Successfully deleted video that doesn't exist! \n${video.name}`,
-        //     status: 'info', // "success" | "error" | "warning" | "info"
-        //     duration: 3000, // milliseconds
-        //     isClosable: true,
-        //     position: 'top-right', // "top", "top-right", "bottom-left", etc.
-        //   });
-        //   continue;
-        // }
-
-
-        if(!newVideo || !newVideo.accountName) {
-          continue
-        }
-        const updateVideoResponse = await fetch('/api/campaign/video/update-info', {method: 'PUT',
-          headers: { 'Content-Type': 'application/json',
-            'Authorization': `Bearer ${idToken}`
-          },
-          body: JSON.stringify({video:newVideo, campaignId}) });
-        if(updateVideoResponse.status !== 200) {
-          console.error('Error updating video');
-          continue
-        }
-        const matchingVideo = videos?.find((vid) =>  vid.link === newVideo.link );
-        if (matchingVideo) {
-          newVideo.userAccountRef = matchingVideo.userAccountRef;
-          newVideo.uid = matchingVideo.uid;
-          newVideo.id = matchingVideo.id;
-        }
-        updatedVideos.push(newVideo);
-        toast({
-          title: 'Success!',
-          description: `Successfully updated video views for \n${video.name}`,
-          status: 'success', // "success" | "error" | "warning" | "info"
-          duration: 3000, // milliseconds
-          isClosable: true,
-          position: 'top-right', // "top", "top-right", "bottom-left", etc.
-        });
-      }
-
-      const response = await fetch('/api/campaign/calculate-progress', {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          campaignId: campaignId,
-        }),
-      })
-      console.log(response)
-      setVideos(updatedVideos);
-    } catch(error) {
-      console.error(error);
-      toast({
-        title: 'Error!',
-        description: 'Error while updating video views.',
-        status: 'error', // "success" | "error" | "warning" | "info"
-        duration: 3000, // milliseconds
-        isClosable: true,
-        position: 'top-right', // "top", "top-right", "bottom-left", etc.
-      });    }
-  }
-
-  const handleUpdateSingleVideo = async (video: IVideo) => {
-    try {
-      const getVideoResponse = await fetch('/api/campaign/video/get-info', {
-        method: 'PUT',
-        body: JSON.stringify({
-          platform: video.link.includes('instagram') ? 'Instagram' : 'TikTok',
-          videoId: video.link.split('/')[5],
-          videoUrl: video.link,
-          api_key: process.env.NEXT_PUBLIC_RAPIDAPI_KEY!,
-        }),
-      });
-
-      if (getVideoResponse.status !== 200) {
-        toast({
-          title: 'Error!',
-          description: `Error getting video info for \n${video.name}`,
-          status: 'error',
-          duration: 3000,
-          isClosable: true,
-          position: 'top-right',
-        });
-        return;
-      }
-
-      const getVideoResponseJson = await getVideoResponse.json();
-      const newVideo = getVideoResponseJson['videoInfo'] as IVideo;
-
-      if (!newVideo || !newVideo.accountName) return;
-
-      await fetch('/api/campaign/video/update-info', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${idToken}`,
-        },
-        body: JSON.stringify({ video: newVideo, campaignId }),
-      });
-
-      setVideos((prev) =>
-        prev
-          ? prev.map((v) => (v.id === video.id ? { ...v, ...newVideo } : v))
-          : null
-      );
-
-      toast({
-        title: 'Updated!',
-        description: `Successfully updated ${video.name}`,
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-        position: 'top-right',
-      });
-    } catch (error) {
-      console.error(error);
-      toast({
-        title: 'Error updating views!',
-        description: `${video.name}`,
-        status: 'error',
-        duration: 3000,
-        isClosable: true,
-        position: 'top-right',
-      });
-    }
-  };
+  const sortedVideos = (videos || []).sort((a, b) => {
+    const field = sortOption;
+    const aDate = new Date(a[field] || 0).getTime();
+    const bDate = new Date(b[field] || 0).getTime();
+    return bDate - aDate;
+  });
 
   return (
     <VStack spacing={6} width="90%" mx="auto" py={6}>
       <Heading textAlign="center" color="green.400">
         {campaign.influencer} - {campaign.activity}
       </Heading>
+
       <HStack spacing={6} w="full" mx="auto" py={6}>
         <Box
           bg="gray.700"
@@ -321,18 +315,51 @@ const AllVideosAdminPage: React.FC<AdminCampaignPageProps> = ({ idToken }) => {
             </StatNumber>
           </Stat>
         </Box>
+
         <Box w="fit-content">
-          <Button colorScheme={"green"} ml={"auto"} size={"lg"} onClick={handleUpdateViews}>Update views</Button>
-
+          <Button
+            colorScheme={'green'}
+            ml={'auto'}
+            size={'lg'}
+            onClick={handleUpdateViews}
+          >
+            Update all views
+          </Button>
         </Box>
-
       </HStack>
 
       <Divider />
 
-      {videos && videos.length > 0 ? (
-        <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={6} w="100%">
-          {videos?.map((video) => (
+      {/* Sort Buttons */}
+      <HStack spacing={4} justify="flex-end" w="full" mb={4}>
+        <Text color="gray.400" fontSize="sm">
+          Sort by:
+        </Text>
+        <Button
+          size="sm"
+          colorScheme={sortOption === 'createdAt' ? 'blue' : 'gray'}
+          variant={sortOption === 'createdAt' ? 'solid' : 'outline'}
+          onClick={() => setSortOption('createdAt')}
+        >
+          Upload Date
+        </Button>
+        <Button
+          size="sm"
+          colorScheme={sortOption === 'lastUpdatedAt' ? 'blue' : 'gray'}
+          variant={sortOption === 'lastUpdatedAt' ? 'solid' : 'outline'}
+          onClick={() => setSortOption('lastUpdatedAt')}
+        >
+          Last Updated
+        </Button>
+      </HStack>
+
+      {sortedVideos && sortedVideos.length > 0 ? (
+        <SimpleGrid
+          columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
+          spacing={6}
+          w="100%"
+        >
+          {sortedVideos.map((video) => (
             <AdminVideoCard
               key={video.id}
               video={video}
